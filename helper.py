248d72e5cb35a838e53143b5ae98ca89befae0a5
Ashraf Kamarudin <ashrafk@runcloud.io>
2021-03-01 23:13:20 +0800

    added comment

--- helper.py
+++ helper.py
@@ -56,6 +56,12 @@ def writeToDocx (toWrite, doc_path):
 
     doc.save(doc_path)
 
+#
+# Set lower boundary of list
+# - Remove elements that is not within new lower boundary
+# - For Example there is a list [0, 1, 2, 3] and a new boundary is set as 2.
+# - New produced list will be [2,3]
+#
 def setListLowerBound (list, lowerBound):
     popidx = 0
     while popidx < (lowerBound - 1):   
@@ -70,9 +76,16 @@ def removeStopWord (stopWordList, sentence):
 
     return sentence
 
+#
+# Perform a reformat to every element in the list from alphabet to numbered list
+# so that each element follows / applies same format of sentences
+# - e.g a) => 17.1
+# - e.g b) => 17.2
+#
 def reformatToNumberedList (list):
     sidx = 0
     sentences = []
+    print(list)
     for index, sentence in enumerate(list):
         if sentence[1] not in alhpabets:
             sentences.append(sentence)
@@ -82,13 +95,21 @@ def reformatToNumberedList (list):
         for index, value in enumerate(alhpabets):
             if f"\t{value})" in sentence:
                 sentence = sentence.replace(f"\t{value})", f"\t{sidx}.{index+1})")
+                print(sentence)
                 sentences.append(sentence)
-
+    # print(sentences)
     return sentences
-
+#
+# Check if the given key exist in the given dictionary
+#
 def checkKey(dict, key): 
     return True if key in dict.keys() else False
 
+#
+# Count the total number of occurance of each word
+# in the given sentence(haystack) and add the count to the
+# count_list(output) and return the new count_list
+#
 def countSingleWord(haystack, output):
     for current in extractWordFromString(haystack):
         if checkKey(output, current):
@@ -98,6 +119,11 @@ def countSingleWord(haystack, output):
 
     return output
 
+#
+# Count the total number of occurance of double word same as countSingleWord
+# e.g => This is a sentence
+# The double word that count is ['this is', 'is a', 'a sentence']
+#
 def countDoubleWord(haystack, output):
     previous = ''
     for current in extractWordFromString(haystack):
@@ -118,6 +144,10 @@ def countDoubleWord(haystack, output):
 def extractWordFromString(haystack):
     return re.sub('['+string.punctuation+']', '', haystack).split()[1:]
 
+#
+# Change format from dictionary to a list
+# with the format [key => value, key => value]
+#
 def transformDictToArrowNotationList(dict):
     var = []
     for key in dict:
